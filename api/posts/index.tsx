25540e97718c6525045a9b87cf93166dd31c871c
Sefa Demirtaş <39422788+tugsef@users.noreply.github.com>
2023-11-22 10:02:18 +0300

    Update
    Start

--- api/posts/index.tsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const fetchGetAllPosts = async (): Promise<any> => {
-  
-  const { data } = await axios.get(
-        "http://localhost:3333/posts"  
-  );
-  return data;
-};
