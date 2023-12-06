46c0e8554de6152663b2f01c5ed6183670a60809
Sefa Demirtaş <39422788+tugsef@users.noreply.github.com>
2023-12-06 14:01:48 +0300

    update

--- .contentlayer/generated/Blog/_index.mjs
+++ .contentlayer/generated/Blog/_index.mjs
@@ -3,20 +3,20 @@
 import javaLambdaExpressions__indexMdx from './Java-lambda-expressions__index.mdx.json' assert { type: 'json' }
 import advancedGitBranchStatsMerge__indexMdx from './advanced-git-branch-stats-merge__index.mdx.json' assert { type: 'json' }
 import bestPracticesForWritingCleanAndMaintainableCode__indexMdx from './best-practices-for-writing-clean-and-maintainable-code__index.mdx.json' assert { type: 'json' }
-import devopsWhatIsThisTr__indexMdx from './devops-what-is-this-tr__index.mdx.json' assert { type: 'json' }
-import buildingProgressiveWebAppsBridgingTheGapBetweenWebAndMobile__indexMdx from './building-progressive-web-apps-bridging-the-gap-between-web-and-mobile__index.mdx.json' assert { type: 'json' }
-import gitCheatSheatTr__indexMdx from './git-cheat-sheat-tr__index.mdx.json' assert { type: 'json' }
 import automatingRepetitiveTasksProductivityHacksForDevelopers__indexMdx from './automating-repetitive-tasks-productivity-hacks-for-developers__index.mdx.json' assert { type: 'json' }
-import javaStreamApiTr__indexMdx from './java-stream-api-tr__index.mdx.json' assert { type: 'json' }
+import gitCheatSheatTr__indexMdx from './git-cheat-sheat-tr__index.mdx.json' assert { type: 'json' }
+import devopsWhatIsThisTr__indexMdx from './devops-what-is-this-tr__index.mdx.json' assert { type: 'json' }
 import gitMergeRebaseSquash__indexMdx from './git-merge-rebase-squash__index.mdx.json' assert { type: 'json' }
+import javaStreamApiTr__indexMdx from './java-stream-api-tr__index.mdx.json' assert { type: 'json' }
+import buildingProgressiveWebAppsBridgingTheGapBetweenWebAndMobile__indexMdx from './building-progressive-web-apps-bridging-the-gap-between-web-and-mobile__index.mdx.json' assert { type: 'json' }
 import jsFrameworksComparison__indexMdx from './js-frameworks-comparison__index.mdx.json' assert { type: 'json' }
 import masteringCssGridLayout__indexMdx from './mastering-css-grid-layout__index.mdx.json' assert { type: 'json' }
 import mindfulnessAndMeditationTechniquesForDevelopersImprovingFocusAndClarity__indexMdx from './mindfulness-and-meditation-techniques-for-developers-improving-focus-and-clarity__index.mdx.json' assert { type: 'json' }
 import nestjsBlogBackend__indexMdx from './nestjs-blog-backend__index.mdx.json' assert { type: 'json' }
 import nextBlogTailwindcssContentlayerTypscript__indexMdx from './next-blog-tailwindcss-contentlayer-typscript__index.mdx.json' assert { type: 'json' }
 import nextBlogTailwindcssContentlayerTypscriptEng__indexMdx from './next-blog-tailwindcss-contentlayer-typscript-eng__index.mdx.json' assert { type: 'json' }
-import webAccessibilityBestPractices__indexMdx from './web-accessibility-best-practices__index.mdx.json' assert { type: 'json' }
 import restfulApisBuildingBlocks__indexMdx from './restful-apis-building-blocks__index.mdx.json' assert { type: 'json' }
+import webAccessibilityBestPractices__indexMdx from './web-accessibility-best-practices__index.mdx.json' assert { type: 'json' }
 import progressiveWebAppsPwa__indexMdx from './progressive-web-apps-pwa__index.mdx.json' assert { type: 'json' }
 import webAccessibilityInclusiveDesign__indexMdx from './web-accessibility-inclusive-design__index.mdx.json' assert { type: 'json' }
 import webDevelopmentToolsProductivity__indexMdx from './web-development-tools-productivity__index.mdx.json' assert { type: 'json' }
@@ -24,4 +24,4 @@ import whatsIsDocker__indexMdx from './whats-is-docker__index.mdx.json' assert {
 import wordSearchBackendJava__indexMdx from './word-search-backend-java__index.mdx.json' assert { type: 'json' }
 import wordSearchNodejs__indexMdx from './word-search-nodejs__index.mdx.json' assert { type: 'json' }
 
-export const allBlogs = [javaLambdaExpressions__indexMdx, advancedGitBranchStatsMerge__indexMdx, bestPracticesForWritingCleanAndMaintainableCode__indexMdx, devopsWhatIsThisTr__indexMdx, buildingProgressiveWebAppsBridgingTheGapBetweenWebAndMobile__indexMdx, gitCheatSheatTr__indexMdx, automatingRepetitiveTasksProductivityHacksForDevelopers__indexMdx, javaStreamApiTr__indexMdx, gitMergeRebaseSquash__indexMdx, jsFrameworksComparison__indexMdx, masteringCssGridLayout__indexMdx, mindfulnessAndMeditationTechniquesForDevelopersImprovingFocusAndClarity__indexMdx, nestjsBlogBackend__indexMdx, nextBlogTailwindcssContentlayerTypscript__indexMdx, nextBlogTailwindcssContentlayerTypscriptEng__indexMdx, webAccessibilityBestPractices__indexMdx, restfulApisBuildingBlocks__indexMdx, progressiveWebAppsPwa__indexMdx, webAccessibilityInclusiveDesign__indexMdx, webDevelopmentToolsProductivity__indexMdx, whatsIsDocker__indexMdx, wordSearchBackendJava__indexMdx, wordSearchNodejs__indexMdx]
+export const allBlogs = [javaLambdaExpressions__indexMdx, advancedGitBranchStatsMerge__indexMdx, bestPracticesForWritingCleanAndMaintainableCode__indexMdx, automatingRepetitiveTasksProductivityHacksForDevelopers__indexMdx, gitCheatSheatTr__indexMdx, devopsWhatIsThisTr__indexMdx, gitMergeRebaseSquash__indexMdx, javaStreamApiTr__indexMdx, buildingProgressiveWebAppsBridgingTheGapBetweenWebAndMobile__indexMdx, jsFrameworksComparison__indexMdx, masteringCssGridLayout__indexMdx, mindfulnessAndMeditationTechniquesForDevelopersImprovingFocusAndClarity__indexMdx, nestjsBlogBackend__indexMdx, nextBlogTailwindcssContentlayerTypscript__indexMdx, nextBlogTailwindcssContentlayerTypscriptEng__indexMdx, restfulApisBuildingBlocks__indexMdx, webAccessibilityBestPractices__indexMdx, progressiveWebAppsPwa__indexMdx, webAccessibilityInclusiveDesign__indexMdx, webDevelopmentToolsProductivity__indexMdx, whatsIsDocker__indexMdx, wordSearchBackendJava__indexMdx, wordSearchNodejs__indexMdx]
--- components/Home/HomeCoverSection/index.tsx
+++ components/Home/HomeCoverSection/index.tsx
@@ -1,9 +1,7 @@
 "use client";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 import React, { useEffect, useState } from "react";
-import ErrorCoverSection from "./ErrorCoverSection";
 import HomeCoverItem from "./HomeCoverItem"; 
+import data from "@/public/data/news/news.json"
 export interface Source {
   id?: number | string | null;
   name?: number | string | null;
@@ -13,7 +11,7 @@ export interface News {
   author: string;
   content: string;
   description: string;
-  publishedAt: Date;
+  publishedAt: string;
   source?: Source;
   title?: string;
   url?: string;
@@ -21,22 +19,23 @@ export interface News {
 }
 function HomeCoverSection() {
   
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["newsPapers"],
-    queryFn: async () => {
-      const { data } = await axios.get(
-        "https://newsapi.org/v2/top-headlines?country=us&apiKey=72a3f19c66df4771b3d76c6b8e5416a6&category=technology&source=google-news"
-      );
+//   const { data, isLoading, isError } = useQuery({
+//     queryKey: ["newsPapers"],
+//     queryFn: async () => {
+//       const { data } = await axios.get(
+//         "https://newsapi.org/v2/top-headlines?country=us&apiKey=72a3f19c66df4771b3d76c6b8e5416a6&category=technology&source=google-news"
+//       );
 
-      return data.articles as News[] ;
-    },
-  });
+// console.log(data);
+//       return data.articles as News[] ;
+//     },
+//   });
 
-  if (isLoading || isError) return <ErrorCoverSection />;
+//   if (isLoading || isError) return <ErrorCoverSection />;
 
   return (
   <div className="py-2  ms:py-6  ">
-  <HomeCoverItem data={data} />
+  <HomeCoverItem data={data as News[]} />
   </div>
   );
 }
