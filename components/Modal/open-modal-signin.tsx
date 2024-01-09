cd216f8ba63234cf3eaa4da73aac16534a459af1
Sefa Demirtaş <39422788+tugsef@users.noreply.github.com>
2024-01-09 21:49:29 +0300

    Main | update

--- components/Modal/open-modal-signin.tsx
+++ components/Modal/open-modal-signin.tsx
@@ -4,8 +4,8 @@ import cx from "../utils";
 export default function OpenModalSignIn({className}:{className?:string}) {
   return (
     <motion.div  whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} className="relative z-0">
-      <a  className={cx("text-accent",className)}>
-        SignIn
+      <a  className={cx("text-accent/90 hover:text-accent dark:text-accentDark/90 hover:dark:text-accentDark tracking-wide",className)}>
+        Sign In
       </a>
     </motion.div>
   );
--- components/Modal/open-modal-signup.tsx
+++ components/Modal/open-modal-signup.tsx
@@ -4,8 +4,8 @@ import cx from "../utils";
 export default function OpenModalSignUp({className}:{className?:string}) {
   return (
     <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} className="relative z-0">
-      <a className={cx("hover:scale-105 transition-all ease duration-200 lg:py-2 py-1 lg:px-5 px-2 rounded-full border-black border-2 dark:border-white lg:text-base dark:text-white text-sm",className)} >
-        SingUp
+      <a className={cx("py-2 ring-accent  rounded-full px-3 dark:bg-accent/95 dark:hover:bg-accent bg-accentDark/95 hover:bg-accentDark text-light text-sm tracking-widest",className)} >
+        Sing Up
       </a>
     </motion.div>
   );
