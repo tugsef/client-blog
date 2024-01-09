07c1ed6c3235050bbe94cf567a76b3a132fe72e1
Sefa Demirtaş <39422788+tugsef@users.noreply.github.com>
2024-01-09 13:37:20 +0300

    Main | backe-end-start

--- app/dasboard/user/[id]/page.tsx
+++ app/dasboard/user/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/[...nextauth]/route";
 import { Backend_URL } from "@/lib/Constants";
 import { getServerSession } from "next-auth";
 
